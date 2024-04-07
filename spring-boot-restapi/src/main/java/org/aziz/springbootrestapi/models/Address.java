fe51393f6fc1b09e6d2fa4426a402e2c771011de
azizHr <azizharkati20@gmail.com>
2024-04-07 13:43:56 +0000

    create a Address model

---
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/models/Address.java
@@ -0,0 +1,17 @@
+package org.aziz.springbootrestapi.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+    private String country;
+    private String city;
+    private String street;
+    private String postalCode;
+}
