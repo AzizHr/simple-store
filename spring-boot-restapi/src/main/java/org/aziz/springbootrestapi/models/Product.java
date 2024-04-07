dc83a01e0e7063d258f246296603e753bca91a9c
azizHr <azizharkati20@gmail.com>
2024-04-07 14:32:01 +0000

    annotate Product model with @Entity

--- spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/models/Product.java
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/models/Product.java
@@ -1,17 +1,23 @@
 package org.aziz.springbootrestapi.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
 import java.util.UUID;
 
 @Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String name;
     private double price;
