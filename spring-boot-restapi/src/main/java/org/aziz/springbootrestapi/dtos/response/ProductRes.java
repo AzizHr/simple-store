eca95a6e82fa5424deb586886694d8dbc6289703
azizHr <azizharkati20@gmail.com>
2024-04-07 18:31:33 +0000

    create CategoryRes response

---
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/dtos/response/ProductRes.java
@@ -0,0 +1,25 @@
+package org.aziz.springbootrestapi.dtos.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.aziz.springbootrestapi.models.Category;
+import org.aziz.springbootrestapi.models.Variant;
+import java.util.List;
+import java.util.UUID;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductRes {
+    private UUID id;
+    private String name;
+    private double price;
+    private int quantity;
+    private String description;
+    private String type;
+    private Category category;
+    private List<Variant> variants;
+}
