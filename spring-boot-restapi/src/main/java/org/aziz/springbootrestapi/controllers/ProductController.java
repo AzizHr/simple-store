606e299cb9c9989a18acec0c246c6572bc0ea768
azizHr <azizharkati20@gmail.com>
2024-04-07 14:03:42 +0000

    create Product controller

---
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/controllers/ProductController.java
@@ -0,0 +1,10 @@
+package org.aziz.springbootrestapi.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/products")
+@RequiredArgsConstructor
+public class ProductController {}
