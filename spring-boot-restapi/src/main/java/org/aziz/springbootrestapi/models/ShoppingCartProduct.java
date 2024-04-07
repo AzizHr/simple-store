d7fef85645d5a6723696960c9742c9ff1426e40a
azizHr <azizharkati20@gmail.com>
2024-04-07 14:52:27 +0000

    create ShoppingCartProduct model

---
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/models/ShoppingCartProduct.java
@@ -0,0 +1,24 @@
+package org.aziz.springbootrestapi.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.UUID;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class ShoppingCartProduct {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private int quantity;
+    @ManyToOne()
+    private ShoppingCart shoppingCart;
+    @ManyToOne
+    private Product product;
+}
