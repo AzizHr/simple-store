1864a40d8f74b399c2b4973f352ac60ee4bdfc01
azizHr <azizharkati20@gmail.com>
2024-04-07 18:57:48 +0000

    add validation for ProductReq request

--- spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/dtos/request/ProductReq.java
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/dtos/request/ProductReq.java
@@ -1,5 +1,8 @@
 package org.aziz.springbootrestapi.dtos.request;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,11 +18,19 @@ import java.util.UUID;
 @AllArgsConstructor
 public class ProductReq {
     private UUID id;
+    @NotEmpty(message = "المرجو إضافة إسم المنتج")
     private String name;
+    @NotEmpty(message = "المرجو إضافة ثمن المنتج")
+    @Size(message = "ثمن المنتج يجب أن يكون أكبر من 0 درهم", min = 1)
     private double price;
+    @NotEmpty(message = "المرجو إضافة كمية المنتج")
+    @Size(message = "كمية المنتج يجب أن تكون أكبر من 0", min = 1)
     private int quantity;
+    @NotEmpty(message = "المرجو إضافة وصف للمنتج")
     private String description;
+    @NotEmpty(message = "المرجو إضافة نوع المنتج")
     private String type;
+    @NotEmpty(message = "المرجو إضافة فئة المنتج")
     private UUID categoryId;
     private List<Variant> variants;
 }
