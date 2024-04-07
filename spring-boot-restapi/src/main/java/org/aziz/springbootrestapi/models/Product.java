3e16ea9cba2f0cffdf48f9a711b6b798fc4912f4
azizHr <azizharkati20@gmail.com>
2024-04-07 13:40:23 +0000

    create a Product model

---
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/models/Product.java
@@ -0,0 +1,21 @@
+package org.aziz.springbootrestapi.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    private UUID id;
+    private String name;
+    private double price;
+    private int quantity;
+    private String description;
+    private String type;
+}
