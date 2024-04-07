15e486196fe9a31fa5e818c12c250baffa5938c8
azizHr <azizharkati20@gmail.com>
2024-04-07 18:01:35 +0000

    create shoppingCart field in Order model

--- spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/models/Order.java
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/models/Order.java
@@ -1,9 +1,6 @@
 package org.aziz.springbootrestapi.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -26,4 +23,6 @@ public class Order {
     private LocalDate deliveryDate;
     private double totalAmount;
     private OrderStatus status;
+    @OneToOne(mappedBy = "order")
+    private ShoppingCart shoppingCart;
 }
