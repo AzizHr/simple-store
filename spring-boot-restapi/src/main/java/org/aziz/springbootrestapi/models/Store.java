9d3fd5558139556bf2376b54d18b5042aa7f5e67
azizHr <azizharkati20@gmail.com>
2024-04-07 14:40:41 +0000

    create products field in Store model

--- spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/models/Store.java
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/models/Store.java
@@ -1,13 +1,12 @@
 package org.aziz.springbootrestapi.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 import java.util.UUID;
 
 @Setter
@@ -21,4 +20,6 @@ public class Store {
     private UUID id;
     private String name;
     private String description;
+    @OneToMany(mappedBy = "store")
+    private List<Product> products;
 }
