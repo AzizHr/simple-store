6e05841723439b1c0d4cd82b1201e33a2748c8bc
azizHr <azizharkati20@gmail.com>
2024-04-07 18:34:52 +0000

    add methods in Product service

--- spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/services/ProductService.java
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/services/ProductService.java
@@ -1,4 +1,15 @@
 package org.aziz.springbootrestapi.services;
 
+import org.aziz.springbootrestapi.dtos.request.ProductReq;
+import org.aziz.springbootrestapi.dtos.response.ProductRes;
+import java.util.List;
+import java.util.UUID;
+
 public interface ProductService {
+    ProductRes save(ProductReq product);
+    ProductRes update(ProductReq product);
+    ProductRes findById(UUID id);
+    List<ProductRes> findAll();
+    List<ProductRes> findByCategoryId(UUID categoryId);
+    void deleteById(UUID id);
 }
