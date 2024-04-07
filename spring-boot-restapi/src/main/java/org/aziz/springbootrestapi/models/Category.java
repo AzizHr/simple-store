39915aede49430393f98c1c7b95d2b30062c8f4b
azizHr <azizharkati20@gmail.com>
2024-04-07 14:27:57 +0000

    annotate Category model with @Entity

--- spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/models/Category.java
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/models/Category.java
@@ -1,5 +1,9 @@
 package org.aziz.springbootrestapi.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,7 +15,10 @@ import java.util.UUID;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String name;
     private String description;
