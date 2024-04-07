24b845590e15f71388962f2106a9a3d8b578888d
azizHr <azizharkati20@gmail.com>
2024-04-07 14:07:52 +0000

    create Store controller

---
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/controllers/StoreController.java
@@ -0,0 +1,10 @@
+package org.aziz.springbootrestapi.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/stores")
+@RequiredArgsConstructor
+public class StoreController {}
