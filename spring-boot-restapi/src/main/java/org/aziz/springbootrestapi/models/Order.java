ec33f8273226009af7781d361c253a91eed146c6
azizHr <azizharkati20@gmail.com>
2024-04-07 13:50:08 +0000

    create an Order model

---
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/models/Order.java
@@ -0,0 +1,20 @@
+package org.aziz.springbootrestapi.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+    private UUID id;
+    private LocalDate orderDate;
+    private LocalDate deliveryDate;
+    private double totalAmount;
+}
