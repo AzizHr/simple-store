52753a25bf77c6b81a90901111d8fde32d9fd37c
azizHr <azizharkati20@gmail.com>
2024-04-07 20:23:08 +0000

    remove unused imports from ProductReq

--- spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/dtos/request/ProductReq.java
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/dtos/request/ProductReq.java
@@ -1,14 +1,12 @@
 package org.aziz.springbootrestapi.dtos.request;
 
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.aziz.springbootrestapi.models.Variant;
-
 import java.util.List;
 import java.util.UUID;
 
