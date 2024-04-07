6a9fb9f5c97be01e9752fe66fe8ee4fe619e1842
azizHr <azizharkati20@gmail.com>
2024-04-07 14:34:01 +0000

    create category field in Product model

--- spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/models/Product.java
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/models/Product.java
@@ -1,9 +1,6 @@
 package org.aziz.springbootrestapi.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -24,4 +21,6 @@ public class Product {
     private int quantity;
     private String description;
     private String type;
+    @ManyToOne
+    private Category category;
 }
