006ba0ec960d1fc922cfa4695feb482732f5f815
azizHr <azizharkati20@gmail.com>
2024-04-07 13:46:13 +0000

    create a Store model

---
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/models/Store.java
@@ -0,0 +1,18 @@
+package org.aziz.springbootrestapi.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Store {
+    private UUID id;
+    private String name;
+    private String description;
+}
