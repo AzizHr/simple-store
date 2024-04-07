e724482e090c84551db563c965c5b9729e15283f
azizHr <azizharkati20@gmail.com>
2024-04-07 18:33:04 +0000

    add methods in Category service

--- spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/services/CategoryService.java
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/services/CategoryService.java
@@ -1,4 +1,12 @@
 package org.aziz.springbootrestapi.services;
 
+import org.aziz.springbootrestapi.models.Category;
+import java.util.List;
+import java.util.UUID;
+
 public interface CategoryService {
+    Category save(Category category);
+    Category findById(UUID id);
+    List<Category> findAll();
+    void deleteById(UUID id);
 }
