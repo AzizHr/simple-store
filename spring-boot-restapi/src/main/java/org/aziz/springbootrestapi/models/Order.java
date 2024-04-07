5b4bcc45cb12c870a17ba7c2534170f9739d8191
azizHr <azizharkati20@gmail.com>
2024-04-07 14:19:42 +0000

    create status field in Order model

--- spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/models/Order.java
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/models/Order.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.aziz.springbootrestapi.enums.OrderStatus;
 
 import java.time.LocalDate;
 import java.util.UUID;
@@ -17,4 +18,5 @@ public class Order {
     private LocalDate orderDate;
     private LocalDate deliveryDate;
     private double totalAmount;
+    private OrderStatus status;
 }
