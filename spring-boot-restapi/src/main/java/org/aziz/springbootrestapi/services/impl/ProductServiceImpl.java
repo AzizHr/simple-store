e33feea961ec3168ccf34e8b6c3238f8bcd813ec
azizHr <azizharkati20@gmail.com>
2024-04-08 17:52:26 +0000

    implement all methods again

--- spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/services/impl/ProductServiceImpl.java
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/services/impl/ProductServiceImpl.java
@@ -7,6 +7,8 @@ import org.aziz.springbootrestapi.models.Product;
 import org.aziz.springbootrestapi.repositories.ProductRepository;
 import org.aziz.springbootrestapi.services.ProductService;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.UUID;
@@ -22,5 +24,30 @@ public class ProductServiceImpl implements ProductService {
     public ProductRes save(ProductReq productReq) {
         Product product = modelMapper.map(productReq, Product.class);
         return modelMapper.map(productRepository.save(product), ProductRes.class);
+    }
+
+    @Override
+    public ProductRes update(ProductReq productReq) {
+        return null;
+    }
+
+    @Override
+    public ProductRes findById(UUID id) {
+        return null;
+    }
+
+    @Override
+    public Page<ProductRes> findAll(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public Page<ProductRes> findByCategoryId(UUID categoryId, Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(UUID id) {
+
     }
 }
