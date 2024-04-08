13fee4002e9586bb6223cae048eff0005e6d513e
azizHr <azizharkati20@gmail.com>
2024-04-08 17:53:53 +0000

    implement update prodcut method

--- spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/services/ProductService.java
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/services/ProductService.java
@@ -2,6 +2,7 @@ package org.aziz.springbootrestapi.services;
 
 import org.aziz.springbootrestapi.dtos.request.ProductReq;
 import org.aziz.springbootrestapi.dtos.response.ProductRes;
+import org.aziz.springbootrestapi.exceptions.ItemNotFoundException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -10,7 +11,7 @@ import java.util.UUID;
 
 public interface ProductService {
     ProductRes save(ProductReq productReq);
-    ProductRes update(ProductReq productReq);
+    ProductRes update(ProductReq productReq) throws ItemNotFoundException;
     ProductRes findById(UUID id);
     Page<ProductRes> findAll(Pageable pageable);
     Page<ProductRes> findByCategoryId(UUID categoryId, Pageable pageable);
--- spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/services/impl/ProductServiceImpl.java
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/services/impl/ProductServiceImpl.java
@@ -3,6 +3,7 @@ package org.aziz.springbootrestapi.services.impl;
 import lombok.RequiredArgsConstructor;
 import org.aziz.springbootrestapi.dtos.request.ProductReq;
 import org.aziz.springbootrestapi.dtos.response.ProductRes;
+import org.aziz.springbootrestapi.exceptions.ItemNotFoundException;
 import org.aziz.springbootrestapi.models.Product;
 import org.aziz.springbootrestapi.repositories.ProductRepository;
 import org.aziz.springbootrestapi.services.ProductService;
@@ -27,8 +28,12 @@ public class ProductServiceImpl implements ProductService {
     }
 
     @Override
-    public ProductRes update(ProductReq productReq) {
-        return null;
+    public ProductRes update(ProductReq productReq) throws ItemNotFoundException {
+        if(productRepository.findById(productReq.getId()).isPresent()) {
+            Product product = modelMapper.map(productReq, Product.class);
+            return modelMapper.map(productRepository.save(product), ProductRes.class);
+        }
+        throw new ItemNotFoundException("No product was found with ID "+productReq.getId());
     }
 
     @Override
