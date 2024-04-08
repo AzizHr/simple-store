15b53f51077b5a230d46fc837e4f8d51f6f17576
azizHr <azizharkati20@gmail.com>
2024-04-08 18:18:47 +0000

    create final field productService in ProductController

--- spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/controllers/ProductController.java
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/controllers/ProductController.java
@@ -1,10 +1,15 @@
 package org.aziz.springbootrestapi.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.aziz.springbootrestapi.services.ProductService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/products")
 @RequiredArgsConstructor
-public class ProductController {}
+public class ProductController {
+
+    private final ProductService productService;
+
+}
