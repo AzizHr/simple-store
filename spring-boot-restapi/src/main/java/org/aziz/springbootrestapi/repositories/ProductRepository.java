62f87206dfc932971798255334f399f005fd9184
azizHr <azizharkati20@gmail.com>
2024-04-08 18:01:27 +0000

    create findAllByCAtegoryId method in ProductRepository

--- spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/repositories/ProductRepository.java
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/repositories/ProductRepository.java
@@ -1,7 +1,11 @@
 package org.aziz.springbootrestapi.repositories;
 
 import org.aziz.springbootrestapi.models.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
-public interface ProductRepository extends JpaRepository<Product, UUID> {}
+public interface ProductRepository extends JpaRepository<Product, UUID> {
+    Page<Product> findAllByCategoryId(UUID categoryId, Pageable pageable);
+}
