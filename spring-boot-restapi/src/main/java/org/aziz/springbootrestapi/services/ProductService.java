dcabc876891305cfeb8b0530715d11c763279892
azizHr <azizharkati20@gmail.com>
2024-04-08 17:49:55 +0000

    change return type of findAll products method to Page<ProductRes>

--- spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/services/ProductService.java
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/services/ProductService.java
@@ -2,6 +2,8 @@ package org.aziz.springbootrestapi.services;
 
 import org.aziz.springbootrestapi.dtos.request.ProductReq;
 import org.aziz.springbootrestapi.dtos.response.ProductRes;
+import org.springframework.data.domain.Page;
+
 import java.util.List;
 import java.util.UUID;
 
@@ -9,7 +11,7 @@ public interface ProductService {
     ProductRes save(ProductReq productReq);
     ProductRes update(ProductReq productReq);
     ProductRes findById(UUID id);
-    List<ProductRes> findAll();
+    Page<ProductRes> findAll();
     List<ProductRes> findByCategoryId(UUID categoryId);
     void deleteById(UUID id);
 }
