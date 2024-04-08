29f8280736cb69473b7c54f621f56f7788289e0f
azizHr <azizharkati20@gmail.com>
2024-04-08 18:08:47 +0000

    create route for saving new category

--- spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/controllers/CategoryController.java
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/controllers/CategoryController.java
@@ -1,7 +1,11 @@
 package org.aziz.springbootrestapi.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.aziz.springbootrestapi.models.Category;
 import org.aziz.springbootrestapi.services.CategoryService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoryController {
 
     private final CategoryService categoryService;
+
+    @PostMapping
+    public ResponseEntity<Category> save(@RequestBody Category category) {
+        return ResponseEntity.ok(categoryService.save(category));
+    }
 
 }
