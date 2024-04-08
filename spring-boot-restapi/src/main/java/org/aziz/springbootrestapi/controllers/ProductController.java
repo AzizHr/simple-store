4e65d0d7d314acab7280f6ef6e18b0649de42a4d
azizHr <azizharkati20@gmail.com>
2024-04-08 18:19:40 +0000

    create route for saving new product

--- spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/controllers/ProductController.java
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/controllers/ProductController.java
@@ -1,7 +1,13 @@
 package org.aziz.springbootrestapi.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.aziz.springbootrestapi.dtos.request.ProductReq;
+import org.aziz.springbootrestapi.dtos.response.ProductRes;
+import org.aziz.springbootrestapi.models.Category;
 import org.aziz.springbootrestapi.services.ProductService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +17,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
 
     private final ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<ProductRes> save(@RequestBody ProductReq productReq) {
+        return ResponseEntity.ok(productService.save(productReq));
+    }
 
 }
