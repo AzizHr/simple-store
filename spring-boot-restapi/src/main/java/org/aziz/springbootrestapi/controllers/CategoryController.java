163142b5ce31c5cfcc0ffff70a413fa7f613f47e
azizHr <azizharkati20@gmail.com>
2024-04-08 18:11:32 +0000

    create route for fetching a single category

--- spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/controllers/CategoryController.java
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/controllers/CategoryController.java
@@ -1,10 +1,13 @@
 package org.aziz.springbootrestapi.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.aziz.springbootrestapi.exceptions.ItemNotFoundException;
 import org.aziz.springbootrestapi.models.Category;
 import org.aziz.springbootrestapi.services.CategoryService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/categories")
@@ -21,6 +24,11 @@ public class CategoryController {
     @PutMapping
     public ResponseEntity<Category> update(@RequestBody Category category) {
         return ResponseEntity.ok(categoryService.save(category));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Category> category(@PathVariable UUID id) throws ItemNotFoundException {
+        return ResponseEntity.ok(categoryService.findById(id));
     }
 
 }
