147c1d8cd7c9c196da8c26e2bf96acdd5769355d
azizHr <azizharkati20@gmail.com>
2024-04-08 18:07:04 +0000

    create final field categoryService in CategoryController

--- spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/controllers/CategoryController.java
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/controllers/CategoryController.java
@@ -1,10 +1,15 @@
 package org.aziz.springbootrestapi.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.aziz.springbootrestapi.services.CategoryService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/categories")
 @RequiredArgsConstructor
-public class CategoryController {}
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+}
