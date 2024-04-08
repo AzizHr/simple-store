459ac0e46ded62610496c7c626314bf90c21a6b1
azizHr <azizharkati20@gmail.com>
2024-04-08 00:45:48 +0000

    add @Bean annotation to modelMapper method in the configuration class

--- spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/configs/GlobalConfig.java
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/configs/GlobalConfig.java
@@ -1,11 +1,13 @@
 package org.aziz.springbootrestapi.configs;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GlobalConfig {
 
+    @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
     }
