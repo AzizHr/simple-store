d37acb685b9d8c2e8d419fa6e91f0dd084f9c6b5
azizHr <azizharkati20@gmail.com>
2024-04-09 08:41:32 +0000

    create GlobalExceptionHandler class

---
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/handlers/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package org.aziz.springbootrestapi.handlers;
+
+import org.aziz.springbootrestapi.exceptions.ItemNotFoundException;
+import org.aziz.springbootrestapi.exceptions.ListIsEmptyException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ItemNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleItemNotFoundException(ItemNotFoundException e) {
+        return new ResponseEntity<>(Map.of("message", e.getMessage()), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ListIsEmptyException.class)
+    public ResponseEntity<Map<String, String>> handleListIsEmptyException(ListIsEmptyException e) {
+        return new ResponseEntity<>(Map.of("message", e.getMessage()), HttpStatus.OK);
+    }
+}
