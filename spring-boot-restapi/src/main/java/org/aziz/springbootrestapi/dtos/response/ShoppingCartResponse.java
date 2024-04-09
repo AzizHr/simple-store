e8003c16c1796377307d9e3b806f26ae8c3ea392
azizHr <azizharkati20@gmail.com>
2024-04-09 21:14:55 +0000

    change ShoppingCartRes name to ShoppingCartResponse

--- spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/dtos/response/ShoppingCartResponse.java
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/dtos/response/ShoppingCartResponse.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class ShoppingCartRes {
+public class ShoppingCartResponse {
     private UUID id;
     private String name;
     private List<ProductResponse> products;
