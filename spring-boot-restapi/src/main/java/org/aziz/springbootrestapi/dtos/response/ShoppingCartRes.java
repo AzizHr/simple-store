51e8962b2eaed5d0b7b1d57e7c01fe1657d2e008
azizHr <azizharkati20@gmail.com>
2024-04-09 21:07:31 +0000

    remove unused imports from ShoppingCartRes

--- spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/dtos/response/ShoppingCartRes.java
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/dtos/response/ShoppingCartRes.java
@@ -1,14 +1,9 @@
 package org.aziz.springbootrestapi.dtos.response;
 
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.aziz.springbootrestapi.models.Customer;
-import org.aziz.springbootrestapi.models.Order;
-import org.aziz.springbootrestapi.models.ShoppingCartProduct;
 import java.util.List;
 import java.util.UUID;
 
