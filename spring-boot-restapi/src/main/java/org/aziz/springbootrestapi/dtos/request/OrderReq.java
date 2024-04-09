81ee732e4efb2fc9897c44568f176bbc0cc75965
azizHr <azizharkati20@gmail.com>
2024-04-09 17:55:20 +0000

    create OrderReq request

---
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/dtos/request/OrderReq.java
@@ -0,0 +1,20 @@
+package org.aziz.springbootrestapi.dtos.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderReq {
+    private UUID id;
+    private LocalDate orderDate;
+    private LocalDate deliveryDate;
+    private double totalAmount;
+    private UUID shoppingCartId;
+}
