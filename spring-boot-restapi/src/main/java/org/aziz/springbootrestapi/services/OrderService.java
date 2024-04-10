e49e1d0dda3c8f8bad468ad12511b29ad8052aa7
azizHr <azizharkati20@gmail.com>
2024-04-10 19:48:42 +0000

    implement save order method

--- spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/services/OrderService.java
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/services/OrderService.java
@@ -2,8 +2,9 @@ package org.aziz.springbootrestapi.services;
 
 import org.aziz.springbootrestapi.dtos.request.OrderRequest;
 import org.aziz.springbootrestapi.dtos.response.OrderResponse;
+import org.aziz.springbootrestapi.exceptions.ItemNotFoundException;
 
 public interface OrderService {
-    OrderResponse save(OrderRequest orderRequest);
+    OrderResponse save(OrderRequest orderRequest) throws ItemNotFoundException;
     OrderResponse update(OrderRequest orderRequest);
 }
--- spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/services/impl/OrderServiceImpl.java
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/services/impl/OrderServiceImpl.java
@@ -3,19 +3,39 @@ package org.aziz.springbootrestapi.services.impl;
 import lombok.RequiredArgsConstructor;
 import org.aziz.springbootrestapi.dtos.request.OrderRequest;
 import org.aziz.springbootrestapi.dtos.response.OrderResponse;
+import org.aziz.springbootrestapi.enums.OrderStatus;
+import org.aziz.springbootrestapi.exceptions.ItemNotFoundException;
+import org.aziz.springbootrestapi.models.Order;
 import org.aziz.springbootrestapi.repositories.OrderRepository;
+import org.aziz.springbootrestapi.repositories.ShoppingCartRepository;
 import org.aziz.springbootrestapi.services.OrderService;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import java.time.LocalDate;
 
 @Service
 @RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final OrderRepository orderRepository;
+    private final ShoppingCartRepository shoppingCartRepository;
+    private final ModelMapper modelMapper;
 
     @Override
-    public OrderResponse save(OrderRequest orderRequest) {
-        return null;
+    public OrderResponse save(OrderRequest orderRequest) throws ItemNotFoundException {
+        Order order = modelMapper.map(orderRequest, Order.class);
+        order.setShoppingCart(shoppingCartRepository.findById(
+                orderRequest.getShoppingCartId()
+        ).orElseThrow(()
+                -> new ItemNotFoundException(
+                        "No shoppingCart was found with ID: "+orderRequest.getShoppingCartId()
+        )));
+
+        order.setOrderDate(LocalDate.now());
+        order.setDeliveryDate(LocalDate.now().plusDays(10));
+        order.setStatus(OrderStatus.PENDING);
+
+        return modelMapper.map(orderRepository.save(order), OrderResponse.class);
     }
 
     @Override
