850af57484434b2384285bb24cc12dc33b2539c9
azizHr <azizharkati20@gmail.com>
2024-04-10 18:44:32 +0000

    create methods to be implemented in StoreProductService

--- spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/services/StoreProductService.java
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/services/StoreProductService.java
@@ -1,4 +1,8 @@
 package org.aziz.springbootrestapi.services;
 
+import org.aziz.springbootrestapi.models.StoreProduct;
+
 public interface StoreProductService {
+    String addProductToStore(StoreProduct storeProduct);
+    String removeProductFromStore(StoreProduct storeProduct);
 }
