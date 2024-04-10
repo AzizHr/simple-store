aa24e27293facb45a9d5840cafee370f4ff7fb9d
azizHr <azizharkati20@gmail.com>
2024-04-10 20:35:26 +0000

    implement methods in ShoppingCartProductServiceImpl

--- spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/services/ShoppingCartProductService.java
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/services/ShoppingCartProductService.java
@@ -1,8 +1,9 @@
 package org.aziz.springbootrestapi.services;
 
 import org.aziz.springbootrestapi.dtos.request.ShoppingCartProductRequest;
+import org.aziz.springbootrestapi.exceptions.ItemNotFoundException;
 
 public interface ShoppingCartProductService {
-    String addProductToShoppingCart(ShoppingCartProductRequest shoppingCartProductRequest);
+    String addProductToShoppingCart(ShoppingCartProductRequest shoppingCartProductRequest) throws ItemNotFoundException;
     String removeProductFromShoppingCart(ShoppingCartProductRequest shoppingCartProductRequest);
 }
--- spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/services/impl/ShoppingCartProductServiceImpl.java
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/services/impl/ShoppingCartProductServiceImpl.java
@@ -2,8 +2,13 @@ package org.aziz.springbootrestapi.services.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.aziz.springbootrestapi.dtos.request.ShoppingCartProductRequest;
+import org.aziz.springbootrestapi.exceptions.ItemNotFoundException;
+import org.aziz.springbootrestapi.models.ShoppingCartProduct;
+import org.aziz.springbootrestapi.repositories.ProductRepository;
 import org.aziz.springbootrestapi.repositories.ShoppingCartProductRepository;
+import org.aziz.springbootrestapi.repositories.ShoppingCartRepository;
 import org.aziz.springbootrestapi.services.ShoppingCartProductService;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,14 +16,27 @@ import org.springframework.stereotype.Service;
 public class ShoppingCartProductServiceImpl implements ShoppingCartProductService {
 
     private final ShoppingCartProductRepository shoppingCartProductRepository;
+    private final ShoppingCartRepository shoppingCartRepository;
+    private final ProductRepository productRepository;
+    private final ModelMapper modelMapper;
 
     @Override
-    public String addProductToShoppingCart(ShoppingCartProductRequest shoppingCartProductRequest) {
-        return "";
+    public String addProductToShoppingCart(ShoppingCartProductRequest shoppingCartProductRequest) throws ItemNotFoundException {
+        ShoppingCartProduct shoppingCartProduct = modelMapper.map(shoppingCartProductRequest, ShoppingCartProduct.class);
+
+        shoppingCartProduct.setShoppingCart(shoppingCartRepository.findById(
+                shoppingCartProductRequest.getShoppingCartId()
+        ).orElseThrow(() -> new ItemNotFoundException("ShoppingCart was not found with ID: "+ shoppingCartProductRequest.getShoppingCartId())));
+
+        shoppingCartProduct.setProduct(productRepository.findById(
+                shoppingCartProductRequest.getProductId()
+        ).orElseThrow(() -> new ItemNotFoundException("Product was not found with ID: "+ shoppingCartProductRequest.getProductId())));
+
+        return modelMapper.map(shoppingCartProductRepository.save(shoppingCartProduct), String.class);
     }
 
     @Override
     public String removeProductFromShoppingCart(ShoppingCartProductRequest shoppingCartProductRequest) {
-        return "";
+        shoppingCartProductRepository.delete(modelMapper.map(shoppingCartProductRequest, ShoppingCartProduct.class));
     }
 }
