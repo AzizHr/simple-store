78206e113b677aa7f01c7790f8d985ac39f58043
azizHr <azizharkati20@gmail.com>
2024-04-10 18:51:33 +0000

    implement StoreProductService by StoreProductServiceImpl

--- spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/services/impl/StoreProductServiceImpl.java
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/services/impl/StoreProductServiceImpl.java
@@ -1,4 +1,24 @@
 package org.aziz.springbootrestapi.services.impl;
 
-public class StoreProductServiceImpl {
+import lombok.RequiredArgsConstructor;
+import org.aziz.springbootrestapi.models.StoreProduct;
+import org.aziz.springbootrestapi.repositories.StoreProductRepository;
+import org.aziz.springbootrestapi.services.StoreProductService;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class StoreProductServiceImpl implements StoreProductService {
+
+    private final StoreProductRepository storeProductRepository;
+
+    @Override
+    public String addProductToStore(StoreProduct storeProduct) {
+        return "";
+    }
+
+    @Override
+    public String removeProductFromStore(StoreProduct storeProduct) {
+        return "";
+    }
 }
