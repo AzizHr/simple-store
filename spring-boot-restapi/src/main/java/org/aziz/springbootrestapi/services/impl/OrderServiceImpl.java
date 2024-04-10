e16546afa5b87d773c2b83d586ffe0df9375f57c
azizHr <azizharkati20@gmail.com>
2024-04-10 18:48:14 +0000

    implement OrderServiceImpl by OrderService

--- spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/services/impl/OrderServiceImpl.java
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/services/impl/OrderServiceImpl.java
@@ -1,4 +1,25 @@
 package org.aziz.springbootrestapi.services.impl;
 
-public class OrderServiceImpl {
+import lombok.RequiredArgsConstructor;
+import org.aziz.springbootrestapi.dtos.request.OrderRequest;
+import org.aziz.springbootrestapi.dtos.response.OrderResponse;
+import org.aziz.springbootrestapi.repositories.OrderRepository;
+import org.aziz.springbootrestapi.services.OrderService;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceImpl implements OrderService {
+
+    private final OrderRepository orderRepository;
+
+    @Override
+    public OrderResponse save(OrderRequest orderRequest) {
+        return null;
+    }
+
+    @Override
+    public OrderResponse update(OrderRequest orderRequest) {
+        return null;
+    }
 }
