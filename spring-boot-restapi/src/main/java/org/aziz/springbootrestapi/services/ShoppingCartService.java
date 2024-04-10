0422a1241d41c1eebbd5a2fee7fb1f9d1952b97c
azizHr <azizharkati20@gmail.com>
2024-04-10 20:38:33 +0000

    implement update shoppingCart method

--- spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/services/ShoppingCartService.java
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/services/ShoppingCartService.java
@@ -2,7 +2,8 @@ package org.aziz.springbootrestapi.services;
 
 import org.aziz.springbootrestapi.dtos.request.ShoppingCartRequest;
 import org.aziz.springbootrestapi.dtos.response.ShoppingCartResponse;
+import org.aziz.springbootrestapi.exceptions.ItemNotFoundException;
 
 public interface ShoppingCartService {
-    ShoppingCartResponse updateShoppingCart(ShoppingCartRequest shoppingCartRequest);
+    ShoppingCartResponse updateShoppingCart(ShoppingCartRequest shoppingCartRequest) throws ItemNotFoundException;
 }
--- spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/services/impl/ShoppingCartServiceImpl.java
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/services/impl/ShoppingCartServiceImpl.java
@@ -3,8 +3,11 @@ package org.aziz.springbootrestapi.services.impl;
 import lombok.RequiredArgsConstructor;
 import org.aziz.springbootrestapi.dtos.request.ShoppingCartRequest;
 import org.aziz.springbootrestapi.dtos.response.ShoppingCartResponse;
+import org.aziz.springbootrestapi.exceptions.ItemNotFoundException;
+import org.aziz.springbootrestapi.models.ShoppingCart;
 import org.aziz.springbootrestapi.repositories.ShoppingCartRepository;
 import org.aziz.springbootrestapi.services.ShoppingCartService;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,9 +15,14 @@ import org.springframework.stereotype.Service;
 public class ShoppingCartServiceImpl implements ShoppingCartService {
 
     private final ShoppingCartRepository shoppingCartRepository;
+    private final ModelMapper modelMapper;
 
     @Override
-    public ShoppingCartResponse updateShoppingCart(ShoppingCartRequest shoppingCartRequest) {
-        return null;
+    public ShoppingCartResponse updateShoppingCart(ShoppingCartRequest shoppingCartRequest) throws ItemNotFoundException {
+        if(shoppingCartRepository.findById(shoppingCartRequest.getId()).isPresent()) {
+            ShoppingCart shoppingCart = modelMapper.map(shoppingCartRequest, ShoppingCart.class);
+            return modelMapper.map(shoppingCartRepository.save(shoppingCart), ShoppingCartResponse.class);
+        }
+        throw new ItemNotFoundException("No shoppingCart was found with ID: "+shoppingCartRequest.getId());
     }
 }
