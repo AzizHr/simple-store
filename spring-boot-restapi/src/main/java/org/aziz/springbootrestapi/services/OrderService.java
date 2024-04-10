8f13e2d5aee1021f200395d676330b75ee83c8b7
azizHr <azizharkati20@gmail.com>
2024-04-10 18:38:28 +0000

    create methods to be implemented in OrderService

--- spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/services/OrderService.java
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/services/OrderService.java
@@ -1,4 +1,9 @@
 package org.aziz.springbootrestapi.services;
 
+import org.aziz.springbootrestapi.dtos.request.OrderRequest;
+import org.aziz.springbootrestapi.dtos.response.OrderResponse;
+
 public interface OrderService {
+    OrderResponse save(OrderRequest orderRequest);
+    OrderResponse update(OrderRequest orderRequest);
 }
