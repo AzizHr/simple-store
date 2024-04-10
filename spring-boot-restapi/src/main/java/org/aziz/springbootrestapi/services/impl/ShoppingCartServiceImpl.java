6d5b9128315ca7ed4fdd1025b40ab925eef0692a
azizHr <azizharkati20@gmail.com>
2024-04-10 19:02:48 +0000

    implement ShoppingCartService by ShoppingCartServiceImpl

--- spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/services/impl/ShoppingCartServiceImpl.java
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/services/impl/ShoppingCartServiceImpl.java
@@ -1,4 +1,20 @@
 package org.aziz.springbootrestapi.services.impl;
 
-public class ShoppingCartServiceImpl {
+import lombok.RequiredArgsConstructor;
+import org.aziz.springbootrestapi.dtos.request.ShoppingCartRequest;
+import org.aziz.springbootrestapi.dtos.response.ShoppingCartResponse;
+import org.aziz.springbootrestapi.repositories.ShoppingCartRepository;
+import org.aziz.springbootrestapi.services.ShoppingCartService;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ShoppingCartServiceImpl implements ShoppingCartService {
+
+    private final ShoppingCartRepository shoppingCartRepository;
+
+    @Override
+    public ShoppingCartResponse updateShoppingCart(ShoppingCartRequest shoppingCartRequest) {
+        return null;
+    }
 }
