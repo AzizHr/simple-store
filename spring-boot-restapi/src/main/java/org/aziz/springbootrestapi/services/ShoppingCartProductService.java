431350ab8aaae161299e2ec77a02b755713ef446
azizHr <azizharkati20@gmail.com>
2024-04-10 18:41:49 +0000

    create methods to be implemented in ShoppingCartProductService

--- spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/services/ShoppingCartProductService.java
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/services/ShoppingCartProductService.java
@@ -1,4 +1,9 @@
 package org.aziz.springbootrestapi.services;
 
+import org.aziz.springbootrestapi.dtos.request.ShoppingCartProductRequest;
+
 public interface ShoppingCartProductService {
+    String addProductToShoppingCart(ShoppingCartProductRequest shoppingCartProductRequest);
+    String removeProductFromShoppingCart(ShoppingCartProductRequest shoppingCartProductRequest);
+    String updateQuantity(ShoppingCartProductRequest shoppingCartProductRequest);
 }
