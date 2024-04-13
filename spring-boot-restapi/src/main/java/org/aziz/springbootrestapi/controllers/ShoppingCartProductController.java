ab81b9b802efddda83b2960108b7c483115ccb28
azizHr <azizharkati20@gmail.com>
2024-04-13 11:25:56 +0000

    create a route to add new prouduct to a shoppingCart

--- spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/controllers/ShoppingCartProductController.java
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/controllers/ShoppingCartProductController.java
@@ -1,7 +1,13 @@
 package org.aziz.springbootrestapi.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.aziz.springbootrestapi.dtos.request.ShoppingCartProductRequest;
+import org.aziz.springbootrestapi.dtos.response.ShoppingCartProductResponse;
+import org.aziz.springbootrestapi.exceptions.ItemNotFoundException;
 import org.aziz.springbootrestapi.services.ShoppingCartProductService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/shopping-cart")
 public class ShoppingCartProductController {
     private final ShoppingCartProductService shoppingCartProductService;
+
+    @PostMapping("/add-product")
+    public ResponseEntity<ShoppingCartProductResponse> addProduct(@RequestBody ShoppingCartProductRequest shoppingCartProductRequest) throws ItemNotFoundException {
+        return ResponseEntity.ok(shoppingCartProductService.addProductToShoppingCart(shoppingCartProductRequest));
+    }
 }
