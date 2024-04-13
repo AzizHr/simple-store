80810f1918d14b890e187a205edfef60238d4774
azizHr <azizharkati20@gmail.com>
2024-04-13 11:29:47 +0000

    create a route to update an existing store

--- spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/controllers/StoreController.java
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/controllers/StoreController.java
@@ -1,10 +1,24 @@
 package org.aziz.springbootrestapi.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.aziz.springbootrestapi.dtos.request.StoreRequest;
+import org.aziz.springbootrestapi.dtos.response.StoreResponse;
+import org.aziz.springbootrestapi.exceptions.ItemNotFoundException;
+import org.aziz.springbootrestapi.services.StoreService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/stores")
 @RequiredArgsConstructor
-public class StoreController {}
+public class StoreController {
+    private final StoreService storeService;
+
+    @PutMapping()
+    public ResponseEntity<StoreResponse> update(@RequestBody StoreRequest storeRequest) throws ItemNotFoundException {
+        return ResponseEntity.ok(storeService.update(storeRequest));
+    }
+}
