923f4a98c7bd31473eb7dd3dc1c645a493f4e193
azizHr <azizharkati20@gmail.com>
2024-04-13 11:31:34 +0000

    create a route to add new prouduct to a store

--- spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/controllers/StoreProductController.java
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/controllers/StoreProductController.java
@@ -1,7 +1,13 @@
 package org.aziz.springbootrestapi.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.aziz.springbootrestapi.dtos.request.StoreProductRequest;
+import org.aziz.springbootrestapi.dtos.response.StoreProductResponse;
+import org.aziz.springbootrestapi.exceptions.ItemNotFoundException;
 import org.aziz.springbootrestapi.services.StoreProductService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/store")
 public class StoreProductController {
     private final StoreProductService storeProductService;
+
+    @PostMapping("/add-product")
+    public ResponseEntity<StoreProductResponse> addProduct(@RequestBody StoreProductRequest storeProductRequest) throws ItemNotFoundException {
+        return ResponseEntity.ok(storeProductService.addProductToStore(storeProductRequest));
+    }
 }
