a1b067735d2ab0a6b2a4560b90b70f8114395ddf
azizHr <azizharkati20@gmail.com>
2024-04-13 11:20:41 +0000

    create a route to save new order

--- spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/controllers/OrderController.java
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/controllers/OrderController.java
@@ -1,7 +1,13 @@
 package org.aziz.springbootrestapi.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.aziz.springbootrestapi.dtos.request.OrderRequest;
+import org.aziz.springbootrestapi.dtos.response.OrderResponse;
+import org.aziz.springbootrestapi.exceptions.ItemNotFoundException;
 import org.aziz.springbootrestapi.services.OrderService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/orders")
 public class OrderController {
     private final OrderService orderService;
+
+    @PostMapping
+    public ResponseEntity<OrderResponse> save(@RequestBody OrderRequest orderRequest) throws ItemNotFoundException {
+        return ResponseEntity.ok(orderService.save(orderRequest));
+    }
 }
