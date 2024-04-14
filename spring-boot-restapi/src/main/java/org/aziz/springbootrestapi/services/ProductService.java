56339718aa783fbf67bb227ce810a216779eff70
azizHr <azizharkati20@gmail.com>
2024-04-14 19:03:42 +0100

    add methods to ProductService

--- spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/services/ProductService.java
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/services/ProductService.java
@@ -4,6 +4,7 @@ import org.aziz.springbootrestapi.dtos.request.ProductRequest;
 import org.aziz.springbootrestapi.dtos.response.ProductResponse;
 import org.aziz.springbootrestapi.exceptions.ItemNotFoundException;
 import org.aziz.springbootrestapi.exceptions.ListIsEmptyException;
+import org.aziz.springbootrestapi.models.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -14,6 +15,11 @@ public interface ProductService {
     ProductResponse update(ProductRequest productRequest) throws ItemNotFoundException;
     ProductResponse findById(UUID id) throws ItemNotFoundException;
     Page<ProductResponse> findAll(Pageable pageable) throws ListIsEmptyException;
+    Page<Product> findAllByNameLike(String name, String description, Pageable pageable);
+    Page<Product> filterByCategoryName(String categoryName, Pageable pageable);
+    Page<Product> findAllByCategoryNameLike(String categoryName, Pageable pageable);
+    Page<Product> filterByType(String type, Pageable pageable);
+    Page<Product> findAllByTypeLike(String type, Pageable pageable);
     Page<ProductResponse> findAllByCategoryId(UUID categoryId, Pageable pageable) throws ListIsEmptyException;
     void deleteById(UUID id) throws ItemNotFoundException;
 }
