3326f9ff29c845c2fcb0173e0ad367a1f557bc7f
azizHr <azizharkati20@gmail.com>
2024-04-15 11:36:18 +0100

    create register method in AuthService

--- spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/services/AuthService.java
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/services/AuthService.java
@@ -1,9 +1,12 @@
 package org.aziz.springbootrestapi.services;
 
 import org.aziz.springbootrestapi.dtos.request.AuthRequest;
+import org.aziz.springbootrestapi.dtos.request.RegisterRequest;
 import org.aziz.springbootrestapi.dtos.response.AuthResponse;
+import org.aziz.springbootrestapi.dtos.response.RegisterResponse;
 import org.aziz.springbootrestapi.exceptions.UsernameNotFoundException;
 
 public interface AuthService {
     AuthResponse authenticate(AuthRequest authRequest) throws UsernameNotFoundException;
+    RegisterResponse register(RegisterRequest registerRequest) throws UsernameNotFoundException;
 }
