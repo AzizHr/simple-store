4073e74f385e7ded664a2a9069df3202a38de26f
azizHr <azizharkati20@gmail.com>
2024-04-15 11:33:42 +0100

    delete Customer model

--- spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/repositories/CustomerRepository.java
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/repositories/CustomerRepository.java
@@ -1,6 +1,5 @@
 package org.aziz.springbootrestapi.repositories;
 
-import org.aziz.springbootrestapi.models.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
