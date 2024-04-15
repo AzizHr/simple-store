1424f55999f2847e46711df52ad78c502c1d8f7d
azizHr <azizharkati20@gmail.com>
2024-04-15 10:32:45 +0100

    create findUserByEmail method

--- spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/repositories/UserRepository.java
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/repositories/UserRepository.java
@@ -1,6 +1,10 @@
 package org.aziz.springbootrestapi.repositories;
 
 import org.aziz.springbootrestapi.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+import java.util.UUID;
 
-public interface UserRepository extends JpaRepository<User, UUID> {}
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findUserByEmail(String email);
+}
