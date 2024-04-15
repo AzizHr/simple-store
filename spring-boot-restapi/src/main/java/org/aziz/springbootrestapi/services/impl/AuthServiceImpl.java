6b6e69a364761af90f0424bf987f0688722571c7
azizHr <azizharkati20@gmail.com>
2024-04-15 11:09:14 +0100

    implement authenticate method

--- spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/services/impl/AuthServiceImpl.java
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/services/impl/AuthServiceImpl.java
@@ -1,14 +1,31 @@
 package org.aziz.springbootrestapi.services.impl;
 
+import lombok.RequiredArgsConstructor;
 import org.aziz.springbootrestapi.dtos.request.AuthRequest;
 import org.aziz.springbootrestapi.dtos.response.AuthResponse;
+import org.aziz.springbootrestapi.models.User;
+import org.aziz.springbootrestapi.repositories.UserRepository;
+import org.aziz.springbootrestapi.security.Authenticator;
+import org.aziz.springbootrestapi.security.JwtService;
 import org.aziz.springbootrestapi.services.AuthService;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
+
+    private final UserRepository userRepository;
+    private final JwtService jwtService;
+
     @Override
     public AuthResponse authenticate(AuthRequest authRequest) {
-        return null;
+        User user = userRepository.findUserByEmail(authRequest.getEmail())
+                .orElseThrow(() -> new UsernameNotFoundException("Email or password not valid"));
+
+        Authenticator authenticator = new Authenticator(user);
+        String jwtToken = jwtService.generateToken(authenticator);
+        AuthResponse authResponse = new AuthResponse();
+        authResponse.setToken(jwtToken);
+        return authResponse;
     }
 }
