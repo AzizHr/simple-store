32af6474d584576512b297dd95d8ed9e2bb1bec1
azizHr <azizharkati20@gmail.com>
2024-04-15 10:34:27 +0100

    implement loadUserByUsername method

--- spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/services/impl/CustomUserDetailsServiceImpl.java
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/services/impl/CustomUserDetailsServiceImpl.java
@@ -1,14 +1,22 @@
 package org.aziz.springbootrestapi.services.impl;
 
+import lombok.RequiredArgsConstructor;
+import org.aziz.springbootrestapi.repositories.UserRepository;
+import org.aziz.springbootrestapi.security.Authenticator;
 import org.aziz.springbootrestapi.services.CustomUserDetailsService;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CustomUserDetailsServiceImpl implements CustomUserDetailsService {
+
+    private final UserRepository userRepository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        var user = userRepository.findUserByEmail(username).orElseThrow( () -> new UsernameNotFoundException("No user found"));
+        return new Authenticator(user);
     }
 }
