9e9f3314535494b2e335b8b68f30bd852913d75a
azizHr <azizharkati20@gmail.com>
2024-04-15 11:34:11 +0100

    update User model

--- spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/models/User.java
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/models/User.java
@@ -14,8 +14,7 @@ import java.util.UUID;
 @AllArgsConstructor
 @Entity
 @Table(name = "users")
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
-public abstract class User {
+public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
