04db34593591cf222ab1193232b2936734056fd6
azizHr <azizharkati20@gmail.com>
2024-04-16 20:12:48 +0100

    delete SellerRepository

--- spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/repositories/SellerRepository.java
+++
@@ -1,6 +0,0 @@
-package org.aziz.springbootrestapi.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.UUID;
-
-public interface SellerRepository extends JpaRepository<Seller, UUID> {}
--- spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/services/impl/StoreServiceImpl.java
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/services/impl/StoreServiceImpl.java
@@ -5,7 +5,6 @@ import org.aziz.springbootrestapi.dtos.request.StoreRequest;
 import org.aziz.springbootrestapi.dtos.response.StoreResponse;
 import org.aziz.springbootrestapi.exceptions.ItemNotFoundException;
 import org.aziz.springbootrestapi.models.Store;
-import org.aziz.springbootrestapi.repositories.SellerRepository;
 import org.aziz.springbootrestapi.repositories.StoreRepository;
 import org.aziz.springbootrestapi.services.StoreService;
 import org.springframework.stereotype.Service;
