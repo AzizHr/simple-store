1e9a78cefa21ecf6d7e6e22494297536b1e93c7e
azizHr <azizharkati20@gmail.com>
2024-04-16 19:57:13 +0100

    add OpenAPIDefinition annotation in spring boot app

--- spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/SpringBootRestapiApplication.java
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/SpringBootRestapiApplication.java
@@ -1,9 +1,12 @@
 package org.aziz.springbootrestapi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Simple Store API", version = "1.0", description = "Simple Store"))
 public class SpringBootRestapiApplication {
 
     public static void main(String[] args) {
