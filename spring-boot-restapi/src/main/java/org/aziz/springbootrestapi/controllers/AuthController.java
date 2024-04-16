ff28eba2d5015acf57f8460c06b90d2867a3ee87
azizHr <azizharkati20@gmail.com>
2024-04-16 20:02:35 +0100

    create route for registering users

--- spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/controllers/AuthController.java
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/controllers/AuthController.java
@@ -2,7 +2,10 @@ package org.aziz.springbootrestapi.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.aziz.springbootrestapi.dtos.request.AuthRequest;
+import org.aziz.springbootrestapi.dtos.request.RegisterRequest;
 import org.aziz.springbootrestapi.dtos.response.AuthResponse;
+import org.aziz.springbootrestapi.dtos.response.RegisterResponse;
+import org.aziz.springbootrestapi.exceptions.EmailAlreadyInUseException;
 import org.aziz.springbootrestapi.exceptions.UsernameNotFoundException;
 import org.aziz.springbootrestapi.services.AuthService;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +24,11 @@ public class AuthController {
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest authRequest) throws UsernameNotFoundException {
         return ResponseEntity.ok(authService.authenticate(authRequest));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<RegisterResponse> register(@RequestBody RegisterRequest registerRequest) throws UsernameNotFoundException, EmailAlreadyInUseException {
+        return ResponseEntity.ok(authService.register(registerRequest));
     }
 
 }
