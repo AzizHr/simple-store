f2ec803ee8c321b12d0a2261b5c7ccceddf4bbd1
azizHr <azizharkati20@gmail.com>
2024-04-16 20:01:17 +0100

    create route for logging users

--- spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/controllers/AuthController.java
+++ spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/controllers/AuthController.java
@@ -1,6 +1,13 @@
 package org.aziz.springbootrestapi.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.aziz.springbootrestapi.dtos.request.AuthRequest;
+import org.aziz.springbootrestapi.dtos.response.AuthResponse;
+import org.aziz.springbootrestapi.exceptions.UsernameNotFoundException;
+import org.aziz.springbootrestapi.services.AuthService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
 public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest authRequest) throws UsernameNotFoundException {
+        return ResponseEntity.ok(authService.authenticate(authRequest));
+    }
+
 }
