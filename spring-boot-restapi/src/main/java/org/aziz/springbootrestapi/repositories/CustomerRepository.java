717d0274a3e485b03dc6ff7773f97e283a7316af
azizHr <azizharkati20@gmail.com>
2024-04-16 20:12:01 +0100

    delete CustomerRepository

--- spring-boot-restapi/src/main/java/org/aziz/springbootrestapi/repositories/CustomerRepository.java
+++
@@ -1,6 +0,0 @@
-package org.aziz.springbootrestapi.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.UUID;
-
-public interface CustomerRepository extends JpaRepository<Customer, UUID> {}
